adae2da632bc629627ff6828f0780babb28a9f4e
Juliya <Juliya@mail.com>
2021-02-02 19:20:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function words() {
+    let count = 0;
+    let vowelSearch = ["а", "А", "о", "О", "и", "И", "е", "Е", "ё", "Ё", "э", "Э", "ы", "Ы", "у", "У", "ю", "Ю", "я", "Я"];
+    let text = (" Волны катятся одна за другою \n С плеском и шумом глухим \n Люди проходят ничтожной толпою \n Также один за другим. \n Волнам их воля и холод дороже \n Знойных полудня лучей; \n Люди хотят иметь души… \n И что же? \n Души в них волн холодней!");
+    alert(text);
+    for (var i = 0; i < text.length; i++)
+        for (var vowel = 0; vowel < vowelSearch.length; vowel++)
+            if (text[i] === vowelSearch[vowel]) {
+                ++count;
+                break;
+            }
+    return count ? count : "Нет совпадений";
+    
+}
+alert(words());
